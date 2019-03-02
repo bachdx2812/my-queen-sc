28f218a32fbbe7293b651336b0fcf10ed95685d1
bachdx <bachdx.hut@gmail.com>
2019-03-03 02:59:48 +0900

    add CRUD

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,20 +1,97 @@
 <template>
-  <div>
-    Event
+  <div class="container">
+    <h1 class="title">CRUD</h1>
+    <div class="field">
+      <p class="control">
+        <input class="input" type="text" placeholder="Time" v-model="newEvent.time">
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <input class="input" type="text" placeholder="Title" v-model="newEvent.title">
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <input class="input" type="text" placeholder="Content" v-model="newEvent.content">
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <input class="input" type="text" placeholder="HTML Content" v-model="newEvent.htmlContent">
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <button class="button is-success" @click="createEvent">
+          Add
+        </button>
+      </p>
+    </div>
+    <hr>
+    <table class="table">
+      <thead>
+        <tr>
+          <td width="5%">Index</td>
+          <td width="5%">Time</td>
+          <td width="10%">Title</td>
+          <td width="30%">Content</td>
+          <td width="45%">HTML Content</td>
+          <td width="5%"></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(event, index) in events" v-bind:key="event['.key']">
+          <td>{{ index }}</td>
+          <td>{{ event.time }}</td>
+          <td>{{ event.title }}</td>
+          <td>{{ event.content }}</td>
+          <td><div v-html="event.htmlContent"></div></td>
+          <td>
+            <p class="buttons">
+              <a class="button">
+                Edit
+              </a>
+              <a class="button" @click="deleteEvent(event)">
+                Delete
+              </a>
+            </p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase';
+import { db } from '@/firebase'
+const eventsRef = db.ref('events')
 
 export default {
   created: function() {
-    this.database = firebase.database();
-    this.eventsRef = this.database.ref('events');
   },
   data: function() {
     return {
-      events: []
+      newEvent: {}
+    }
+  },
+  firebase: {
+    events: {
+      source: eventsRef
+    }
+  },
+  methods: {
+    createEvent: function() {
+      eventsRef.push(this.newEvent)
+      alert(' Successfully created new event babe!')
+      this.newEvent = {}
+    },
+    deleteEvent: function(event) {
+      const r = confirm(`You really want to delete : ${event.title} ?`);
+      if (r == true) {
+        eventsRef.child(event['.key']).remove()
+        alert('Successfully deleted event')
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from 'firebase'
+
+import vuefire from 'vuefire'
+Vue.use(vuefire);
+
+import './../node_modules/bulma/css/bulma.css'
 
 Vue.config.productionTip = false
-
-const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-}
-firebase.initializeApp(config);
 
 new Vue({
   router,
